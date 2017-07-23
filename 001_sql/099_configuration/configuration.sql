65356a159ba2435ccf5c115b0458952be24f52ae
BeatTheBookie <andre_doerr@web.de>
2017-07-24 00:09:49 +0200

    Add files via upload

---
+++ 001_sql/099_configuration/configuration.sql
@@ -0,0 +1,20 @@
+
+
+/*****************
+	Schemas
+*****************/
+
+create schema source_files;
+
+create schema stage;
+
+create schema raw_dv;
+
+create schema betting_dv;
+
+create schema betting_mart;
+
+create schema dq_mart;
+
+create schema sandbox;
+
