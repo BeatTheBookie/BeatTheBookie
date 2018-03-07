d2f85a117b6ae6e03322f0c4745c73f3b431e83f
BeatTheBookie <andre_doerr@web.de>
2018-03-07 23:45:54 +0100

    Add files via upload

---
+++ 001_sql/004_betting_vault/functions/function_geo_mean.sql
@@ -0,0 +1,28 @@
+
+/************************************************
+	script for ZIP calculation 
+	with R Package VGAM
+	(zero inflated poisson distribution)
+	
+v1.0:
+	- initial
+
+
+	
+		
+*************************************************/
+
+
+	
+
+create or replace r scalar script betting_dv.r_zip(p_x double, p_mean double,p_prob_extra_zero number) returns double
+as
+library("VGAM")
+run <- function(ctx)
+{
+	dzipois(ctx$p_x, ctx$p_mean, ctx$p_prob_extra_zero)
+}
+/
+;
+
+
