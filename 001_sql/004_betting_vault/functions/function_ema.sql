ebb7a46038977bbdd4fce511e90648823215379c
BeatTheBookie <andre_doerr@web.de>
2018-04-07 10:15:28 +0200

    Update function_ema.sql

--- 001_sql/004_betting_vault/functions/function_ema.sql
+++ 001_sql/004_betting_vault/functions/function_ema.sql
@@ -8,13 +8,18 @@ v1.0:
 
 
 	
+parameter:
+group_param -> the group for which the EMA should be calculated e.g the home team
+order_param -> order criteria for series
+series -> series used for EMA calculation
+	
 		
 *************************************************/
 
 
 	
-create or replace python set script betting_dv.ema(team varchar(1000), match_date timestamp, num_goals double, span double order by match_date) 
-emits (team varchar(1000), match_date timestamp, ema double)
+create or replace python set script betting_dv.ema(group_param varchar(1000), order_param varchar(1000), series double, span double order by order_param) 
+emits (group_param varchar(1000), order_param varchar(1000), series double, ema double)
 as
 
 import pandas as pd
@@ -23,21 +28,21 @@ import numpy as np
 def run(ctx):
 	#create empty data frame with fixed number of rows
 	v_num_rows = ctx.size()
-	v_df = pd.DataFrame(index = np.arange(0, v_num_rows), columns= ['team','match_date','num_goals'])
+	v_df = pd.DataFrame(index = np.arange(0, v_num_rows), columns= ['group_param','order_param','series'])
 
 	#read span parameter
 	v_span = ctx.span
 
 	#read input parameter into data frame
 	for i in np.arange(0, v_num_rows):
-		v_df.loc[i] = [ctx.team, ctx.match_date, ctx.num_goals]
+		v_df.loc[i] = [ctx.group_param, ctx.order_param, ctx.series]
 		ctx.next()
 	
 	#calculate EMA
-	v_df['ema'] = pd.ewma(v_df['num_goals'], span=v_span)
+	v_df['ema'] = pd.ewma(v_df['series'], span=v_span)
 	
 	#return all rows
 	for index, row in v_df.iterrows():
-		ctx.emit(row['team'], row['match_date'], row['ema'])
+		ctx.emit(row['group_param'], row['order_param'], row['series'], row['ema'])
 /
 
