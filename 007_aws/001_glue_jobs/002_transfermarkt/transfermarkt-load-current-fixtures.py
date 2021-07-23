765ea230b2d74c6b87063d5d74e6f88235a29a10
EC2 Default User <ec2-user@ip-172-31-44-46.eu-central-1.compute.internal>
2021-07-23 08:53:13 +0000

    more divisions for curren fixtures

--- 007_aws/001_glue_jobs/002_transfermarkt/transfermarkt-load-current-fixtures.py
+++ 007_aws/001_glue_jobs/002_transfermarkt/transfermarkt-load-current-fixtures.py
@@ -16,12 +16,16 @@ tgt_bucket = 'btb-raw-layer'
 tgt_file_name = 'transfermarkt/current-fixtures/current-fixtures.json'
 
 #
-# Currently just 2. Bundesliga configured as this is
-# missing from other sources
+# List of all URLs and divisions to grap
 #
 
-v_url = 'http://www.transfermarkt.de/2-bundesliga/startseite/wettbewerb/L2'
-v_division = 'D2'
+lst_divisions = [
+            {"url" : "http://www.transfermarkt.de/2-bundesliga/startseite/wettbewerb/L2", "division" : "D2"},
+            {"url" : "http://www.transfermarkt.de/2-bundesliga/startseite/wettbewerb/L1", "division" : "D1"}
+             ]
+
+#v_url = 'http://www.transfermarkt.de/2-bundesliga/startseite/wettbewerb/L2'
+#v_division = 'D2'
 # v_url = 'https://www.transfermarkt.de/premier-league/startseite/wettbewerb/GB1'
 
 data = []
@@ -31,41 +35,53 @@ df_fixtures = pd.DataFrame(columns=['division','match_date','home_team','away_te
 v_user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36'
 v_headers = {'User-Agent': v_user_agent}
 
-#load web site
-v_request = urllib.request.Request(url=v_url,headers=v_headers)
-v_response = urllib.request.urlopen(v_request)
 
-#soup parsing
-soup = bs(v_response, 'html.parser')
-#print(soup.prettify())
-
-#get current matchday and each single match
-v_cur_matchday = soup.find('div', attrs={'id':'spieltagtabs-2'})
-v_match_info = v_cur_matchday.findAll('tr', attrs={'class':'begegnungZeile'})
-
-i=0
-
-#loop throw all match infos
-while i < len(v_match_info):
-    #extract single values
-    home_team = v_match_info[i].findAll('td', attrs={'class': 'verein-heim'})
-    away_team = v_match_info[i].findAll('td', attrs={'class': 'verein-gast'})
-    match_date = v_match_info[i].findAll('span', attrs={'class': 'spielzeitpunkt'})
-
-    if len(match_date) == 1:
-        last_match_date =  match_date
-    else:
-        match_date = last_match_date
-
-    #print(home_team[0].text.strip() + ' - ' + away_team[0].text.strip() + ' (' + match_date[0].text.strip() + ' )')
-
-    new_row = {'division':v_division, 'match_date':match_date[0].text.strip(), 'home_team':home_team[0].text.strip(), 'away_team':away_team[0].text.strip()}
-    df_fixtures = df_fixtures.append(new_row, ignore_index=True)
+for division in lst_divisions:
     
-    #data.append([v_division, match_date[0].text.strip(), home_team[0].text.strip(), away_team[0].text.strip()])
-    
+    print('getting matches for division ' + str(division["division"]))
 
-    i += 1
+    #load web site
+    v_request = urllib.request.Request(url=division["url"],headers=v_headers)
+    v_response = urllib.request.urlopen(v_request)
+
+    #soup parsing
+    soup = bs(v_response, 'html.parser')
+    #print(soup.prettify())
+
+    #get current matchday and each single match
+    v_matchdays = soup.findAll('div', attrs={'id':'spieltagsbox'})
+    
+    j=0
+    
+    while j < len(v_matchdays):
+        
+        print('...getting matches from tab ' + str(j))
+    
+        v_match_info = v_matchdays[j].findAll('tr', attrs={'class':'begegnungZeile'})
+    
+        i=0
+    
+        #loop throw all match infos
+        while i < len(v_match_info):
+            #extract single values
+            home_team = v_match_info[i].findAll('td', attrs={'class': 'verein-heim'})
+            away_team = v_match_info[i].findAll('td', attrs={'class': 'verein-gast'})
+            match_date = v_match_info[i].findAll('span', attrs={'class': 'spielzeitpunkt'})
+        
+            if len(match_date) == 1:
+                last_match_date =  match_date
+            else:
+                match_date = last_match_date
+        
+            #print(home_team[0].text.strip() + ' - ' + away_team[0].text.strip() + ' (' + match_date[0].text.strip() + ' )')
+        
+            new_row = {'division':division["division"], 'match_date':match_date[0].text.strip(), 'home_team':home_team[0].text.strip(), 'away_team':away_team[0].text.strip()}
+            df_fixtures = df_fixtures.append(new_row, ignore_index=True)
+            
+        
+            i += 1
+        
+        j += 1
     
     
 #df_data = pd.DataFrame(data)
